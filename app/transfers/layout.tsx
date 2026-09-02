70bc511f6bd0d18d63d14a3307306375318d6437
Anas-mehdy <57631085+Anas-mehdy@users.noreply.github.com>
2026-09-02 14:10:35 +0300

    ui: move new transfer panel to the right

---
+++ app/transfers/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./transfers-position.css";
+
+export default function TransfersLayout({ children }: { children: ReactNode }) {
+  return children;
+}
